22b37c93ac0f3a821fb8df0830549c95bca36644
Naor Farhi <naor0208@gmail.com>
2020-12-20 15:08:23 +0200

    All files

---
+++ dist/script.js
@@ -0,0 +1,49 @@
+// Variables
+const body = document.querySelector("body");
+const ulNavBar = document.querySelector(".ulNavBar");
+const navLinks = document.querySelectorAll(".link");
+const pages = document.querySelectorAll(".page");
+
+// Events
+ulNavBar.addEventListener("click", choosePage);
+
+// Window events to know when the screen is big enough for ul
+window.addEventListener("load", checkWidth);
+window.onresize = function windowResized() {
+  checkWidth()
+}
+
+function checkWidth() {
+  if (window.innerWidth > 1000) {
+    document.querySelector(".toggler").checked = true;
+  }
+  else{
+    check();
+  }
+}
+
+function check() {
+  document.querySelector(".toggler").checked = false;
+}
+
+
+function choosePage(e) {
+  const eventTargetParent = e.target.parentElement;
+  if (eventTargetParent.classList.contains("link")) {
+    // Checkbox checks to hide it
+    checkWidth();
+    pages.forEach((page) => {
+      // Setting each page to dispaly none
+      page.style.display = "none";
+      if (page.classList.contains(eventTargetParent.classList[0])) {
+        // Removes display none from the page that was chosen
+        page.removeAttribute("style");
+      }
+    });
+  }
+  // Stops from sending # to URL
+  e.preventDefault();
+}
+
+
+
